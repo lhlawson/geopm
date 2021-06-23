b3b464f12ddf74b20689fb801d5da97ae3027b7f
lowren.h.lawson@intel.com <lowren.h.lawson@intel.com>
2021-06-23 16:37:49 -0700

    Added OMPI Exclude list launcher fix
    Signed-off-by: lowren.h.lawson@intel.com <lowren.h.lawson@intel.com>

--- scripts/geopmpy/launcher.py
+++ scripts/geopmpy/launcher.py
@@ -1311,6 +1311,9 @@ class OMPIExecLauncher(Launcher):
         """
         return ','
 
+    def exclude_list_option(self):
+        return []
+
 class IMPIExecLauncher(Launcher):
     """
     Launcher derived object for use with the Intel(R) MPI Library job launch
